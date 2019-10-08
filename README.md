967a06b5c4a6cadff9a0a07958ed9a252a739133
Aaron Freedman <aaron.freedman2@gmail.com>
2019-10-07 21:16:56 -0400

    Add some new functions and readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## Comedy Web
 
 An analysis of comedy actors and their most frequent collaborators, using IMDB data.
+
+## Research Questions
+
+### What are the most central communities within this network?
+We tried a few out of the box clique / community clustering algorithms, but with no obvious insights. We need to find a way to filter out large cliques that are only connected by a single movie.
+
+### Who is the most well connected actor?
+Will Ferrell (see `get_most_connected_actor`)
+TODO: see which movies connect him to other actors.
+
+### What are some fun shortest path examples?
+Think of random actor pairings and run shortest path on them.
--- network.py
+++ network.py
@@ -3,30 +3,31 @@ import pandas as pd
 import itertools
 from collections import defaultdict
 import json
+import operator
 
 # network stuff
 import networkx as nx
-from pyvis.network import Network
+# from pyvis.network import Network
 
 
 def create_collab_dict(movie, collab_dict={}):
     """
     Takes in a movie dataframe with a row per actor and outputs a dictionary with a key representing a pair
-    
+
     {
       ('Mark Wahlberg', 'Will Ferrell'): {
-          'counts': 3, 
+          'counts': 3,
           'movies':['The Other Guys', 'Daddy's Home', 'Daddy's Home 2']
           }
     }
-    
+
     """
     pair_list = itertools.combinations(movie['actor_name'], 2)
     for pair in pair_list:
         collab_dict.setdefault(pair, {'weight': 0, 'movie_list': []})
         collab_dict[pair]['weight'] += 1
         collab_dict[pair]['movie_list'].append(movie['primaryTitle'].iloc[0])
-    
+
     return collab_dict
 
 
@@ -49,18 +50,18 @@ def create_edges(df_main, movie_list):
 
         if counter == len(movie_list):
             print(f'Finished parsing {len(movie_list)} movies')
-    
+
     return edge_dict
 
 
 def create_network_graph(data_path):
     print(f'Loading data from {data_path}')
     df = pd.read_csv(data_path)
-    
+
     # create actor df
     df_actor = df['primaryName'].value_counts().reset_index()
     df_actor.columns = ['actor_name', 'num_movies']
-    
+
     # only look at actors who are in more than 5 movies
     df_actor_subset = df_actor[df_actor['num_movies'] > 5]
 
@@ -77,7 +78,33 @@ def create_network_graph(data_path):
     return G
 
 
+def get_most_connected_actor(G):
+    centrality = nx.degree_centrality(G)
+    top_pairs = sorted(centrality.items(), key=operator.itemgetter(1), reverse=True)
+
+    for name, pct_nodes in top_pairs[:5]:
+        print(name, pct_nodes)
+
+
+def get_cliques(G):
+    cliques = []
+    for clique in nx.find_cliques(G):
+        if len(clique) > 2:
+            cliques.append((clique, len(clique)))
+
+    import pdb; pdb.set_trace()
+
+    for c, c_size in sorted(cliques, key=operator.itemgetter(1), reverse=True)[:10]:
+        print(c, c_size)
+
+
 if __name__ == "__main__":
     G = create_network_graph('data/comedies_actors.csv')
     top_pairs = sorted(G.edges.data('weight'), key=lambda r: r[2], reverse=True)[:3]
     print(f"Top Actor Pairs: {top_pairs}")
+
+    # get_most_connected_actor(G)
+    get_cliques(G)
+
+
+    import pdb; pdb.set_trace()
